4f7c9460d8e16f76d56cb757e01be1c8de60fdf8
Clay Karwisch <clay.karwisch@gmail.com>
2020-11-13 14:21:36 -0500

    add input validations

--- todo.rb
+++ todo.rb
@@ -1,7 +1,50 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 
-get "/" do
-  erb "You have no lists.", layout: :layout
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get '/' do
+  redirect '/lists'
+end
+
+# View all lists
+get '/lists' do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get '/lists/new' do
+  erb :new_list, layout: :layout
+end
+
+# Create a new list
+post '/lists' do
+  list_name = params[:list_name].strip
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list was sucessfully created'
+    redirect '/lists'
+  end
+end
+
+# Returns an error message if name is invalid. Returns nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover?(name.size)
+    'The list name must be between 1 and 100 characters'
+  elsif session[:lists].any? { |list| list[:name] == name }
+    'The list name must be unique'
+  end
 end
