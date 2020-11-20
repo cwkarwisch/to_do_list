7021ce010164f8f10d255c5622c96c29e9c0f388
Clay Karwisch <clay.karwisch@gmail.com>
2020-11-20 10:05:55 -0500

    add unique ids to lists and tasks

--- todo.rb
+++ todo.rb
@@ -3,6 +3,8 @@ require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 require 'sinatra/content_for'
 
+DIGITS = '([0-9]+)'
+
 configure do
   enable :sessions
   set :session_secret, 'secret'
@@ -14,12 +16,12 @@ before do
   @lists = session[:lists]
 end
 
-before '/lists/:list_id*' do
-  @list_id = params[:list_id].to_i
+before /\/lists\/#{DIGITS}/ do
+  @list_id = params['captures'].first.to_i
   @list = load_list(@list_id)
 end
 
-before '/lists/:list_id/:tasks/:task_id' do
+before '/lists/:list_id/:tasks/:task_id*' do
   @task_id = params[:task_id].to_i
 end
 
@@ -49,20 +51,25 @@ end
 
 # Delete an individual list
 post '/lists/:list_id/delete' do
-  session[:lists].delete_at(@list_id)
-  session[:success] = 'The list was successfully deleted.'
-  redirect '/lists'
+  session[:lists].delete_if { |list| list[:id] == @list_id }
+  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+    "/lists"
+  else
+    session[:success] = 'The list was successfully deleted.'
+    redirect '/lists'
+  end
 end
 
 # Add a task to an existing list
 post '/lists/:list_id/task' do
   task_name = params[:task].strip
-  error = error_for_task_name(task_name, @list_id)
+  error = error_for_task_name(task_name)
   if error
     session[:error] = error
     erb :single_list, layout: :layout
   else
-    @list[:todos] << { name: task_name, completed: false }
+    id = next_task_id(@list)
+    @list[:todos] << { name: task_name, completed: false, id: id }
     session[:success] = 'The task was successfully added.'
     redirect "/lists/#{@list_id}"
   end
@@ -76,7 +83,7 @@ post '/lists/:list_id' do
     session[:error] = error
     erb :edit_list, layout: :layout
   else
-    session[:lists][@list_id][:name] = new_list_name
+    session[:lists].find { |list| list[:id] == @list_id }[:name] = new_list_name
     session[:success] = 'The name of the list was sucessfully changed.'
     redirect "/lists/#{@list_id}"
   end
@@ -84,8 +91,8 @@ end
 
 # Update completed status of an existing task
 post '/lists/:list_id/tasks/:task_id' do
-  task = session[:lists][@list_id][:todos][@task_id]
-  params[:completed] == "true" ? task[:completed] = true : task[:completed] = false
+  task = load_task(@list_id, @task_id)
+  task[:completed] = params[:completed] == "true" ? true : false
   redirect "/lists/#{@list_id}"
 end
 
@@ -98,11 +105,14 @@ post '/lists/:list_id/complete_all' do
 end
 
 # Delete a task from an existing list
-post '/lists/:list_id/delete_task/:task_id' do
-  @list = session[:lists][@list_id]
-  @list[:todos].delete_at(@task_id)
-  session[:success] = "The task has been deleted."
-  redirect "/lists/#{@list_id}"
+post '/lists/:list_id/tasks/:task_id/delete' do
+  @list[:todos].delete_if { |task| task[:id] == @task_id }
+  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+    status 204
+  else
+    session[:success] = "The task has been deleted."
+    redirect "/lists/#{@list_id}"
+  end
 end
 
 # Create a new list
@@ -113,7 +123,8 @@ post '/lists' do
     session[:error] = error
     erb :new_list, layout: :layout
   else
-    session[:lists] << { name: list_name, todos: [] }
+    id = next_list_id
+    session[:lists] << { name: list_name, todos: [], id: id }
     session[:success] = 'The list was sucessfully created.'
     redirect '/lists'
   end
@@ -129,15 +140,15 @@ def error_for_list_name(name)
 end
 
 # Returns an error message if task name is invalid. Returns nil if name valid.
-def error_for_task_name(name, list_number)
+def error_for_task_name(name)
   if !(1..100).cover?(name.size)
     'The task name must be between 1 and 100 characters.'
   end
 end
 
 def load_list(list_id)
-  if session[:lists][list_id]
-    list = session[:lists][list_id]
+  list = session[:lists].find { |list| list[:id] == list_id }
+  if list
     return list
   else
     session[:error] = 'The specified list was not found.'
@@ -145,9 +156,33 @@ def load_list(list_id)
   end
 end
 
+def load_task(list_id, task_id)
+  task = session[:lists].find { |list| list[:id] == list_id }[:todos].find { |task| task[:id] == task_id }
+  if task
+    return task
+  else
+    session[:error] = 'The specified task was not found.'
+    redirect "/lists/#{list_id}"
+  end
+end
+
+def next_list_id
+  max = session[:lists].map { |list| list[:id] }.max || 0
+  max + 1
+end
+
+def next_task_id(list)
+  max = list[:todos].map { |task| task[:id] }.max || 0
+  max + 1
+end
+
 helpers do
   def list_complete?(list)
     tasks_count(list) > 0 && incomplete_tasks_count(list) == 0
+  end
+
+  def task_complete?(task)
+    task[:completed] == true
   end
 
   def incomplete_tasks_count(list)
@@ -162,15 +197,19 @@ helpers do
     "complete" if list_complete?(list)
   end
 
+  def task_class(task)
+    "complete" if task_complete?(task)
+  end
+
   def sorted_lists(lists, &block)
     complete, incomplete = lists.partition { |list| list_complete?(list) }
-    incomplete.each { |list| yield(list, lists.find_index(list)) }
-    complete.each { |list| yield(list, lists.find_index(list)) }
+    incomplete.each { |list| yield(list) }
+    complete.each { |list| yield(list) }
   end
 
   def sorted_tasks(tasks, &block)
     complete, incomplete = tasks.partition { |task| task[:completed] }
-    incomplete.each { |task| yield(task, tasks.find_index(task)) }
-    complete.each { |task| yield(task, tasks.find_index(task)) }
+    incomplete.each { |task| yield(task, task[:id]) }
+    complete.each { |task| yield(task, task[:id]) }
   end
 end
