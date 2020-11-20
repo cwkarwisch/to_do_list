39ab834110b7541e1998463b9b546f661843fcec
Clay Karwisch <clay.karwisch@gmail.com>
2020-11-20 10:02:37 -0500

    add js to confirm permanent deletion of tasks

---
+++ public/javascripts/application.js
@@ -0,0 +1,26 @@
+$(function() {
+
+  $("form.delete").submit(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    let ok = confirm("Are you sure you want to permanently delete this?");
+    if (ok) {
+      let form = $(this);
+
+      let request = $.ajax({
+        url: form.attr("action"),
+        method: form.attr("method")
+      });
+
+      request.done(function(data, textStatus, jqXHR) {
+        if (jqXHR.status === 204) {
+          form.parent("li").remove();
+        } else if (jqXHR.status === 200) {
+          document.location = data;
+        }
+      });
+    }
+  });
+
+});
